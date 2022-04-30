9e13088d24cc8102d1cadb38723a88c0b272f5ab
Starovoytov Alexandr <saniy200311@gmail.com>
2022-04-30 14:58:48 +0300

    Переписана лаба №10

--- lab10/main.cpp
+++ lab10/main.cpp
@@ -5,16 +5,33 @@
 int main()
 {
     lab10::SetSeq<int> test { { 1, 2, 3 }, { 3, 4, 5 }, { 4 }, { 1, 2, 3 }, { 1 } };
-    for (auto it = test.cbeginIntersections(); it != test.cendIntersections(); ++it) {
-        for (auto& el : *it) {
+    std::cout << "intersections:\n";
+    for (auto& intersection : test) {
+        for (auto& el : intersection) {
             std::cout << el << ' ';
         }
         std::cout << '\n';
     }
-    std::cout << "\ntest[1]: ";
-    for (auto& el : test[1]) {
+    std::cout << "test[1]: ";
+    for (auto& el : (std::set<int>)test[1]) {
         std::cout << el << ' ';
     }
     std::cout << '\n';
+
+    test[1].insert(2);
+
+    std::cout << "test[1], after insert(2): ";
+    for (auto& el : (std::set<int>)test[1]) {
+        std::cout << el << ' ';
+    }
+    std::cout << '\n';
+
+    std::cout << "intersections:\n";
+    for (auto& intersection : test) {
+        for (auto& el : intersection) {
+            std::cout << el << ' ';
+        }
+        std::cout << '\n';
+    }
     return 0;
 }
--- lab10/set_sequence.hpp
+++ lab10/set_sequence.hpp
@@ -10,47 +10,61 @@
 namespace lab10 {
 template <typename T, typename Container = std::vector<std::set<T>>>
 class SetSeq {
-public:
-    using container_type = Container;
-    using value_type = typename Container::value_type;
-    using size_type = typename Container::size_type;
-    using const_reference = typename Container::const_reference;
-    using const_iterator = typename Container::const_iterator;
-
 private:
-    container_type seq = container_type();
-    container_type pairsIntersecions = container_type();
+    /* Последовательность множеств */
+    Container seq = Container();
+    /* Пересечения соседних множеств */
+    Container pairsIntersections;
 
-private:
+    /* Вычисление всех пересечений */
     void calcIntersections()
     {
         std::ranges::transform(seq | std::ranges::views::take(seq.size() - 1),
             seq | std::ranges::views::drop(1),
-            std::back_inserter(pairsIntersecions),
+            std::back_inserter(pairsIntersections),
             [](const auto& a, const auto& b) {
-                value_type res;
+                std::set<T> res;
                 std::ranges::set_intersection(a, b, std::inserter(res, res.end()));
                 return res;
             });
     }
 
+    /* Вычисление пересечений, в которые входит множество под номером index */
+    void calcIntersections(std::size_t index)
+    {
+        if (index != 0) {
+            auto& intersection = pairsIntersections[index - 1];
+            intersection.clear();
+            std::ranges::set_intersection(seq[index - 1], seq[index], std::inserter(intersection, intersection.end()));
+        }
+        if (index != size() - 1) {
+            auto& intersection = pairsIntersections[index];
+            intersection.clear();
+            std::ranges::set_intersection(seq[index], seq[index + 1], std::inserter(intersection, intersection.end()));
+        }
+    }
+
 public:
     SetSeq() = default;
-    explicit SetSeq(const container_type& cont)
+
+    explicit SetSeq(const Container& cont)
         : seq(cont)
     {
         calcIntersections();
     }
-    explicit SetSeq(container_type&& cont)
+
+    explicit SetSeq(Container&& cont)
         : seq(std::move(cont))
     {
         calcIntersections();
     }
+
     SetSeq(const SetSeq& other) = default;
     SetSeq(SetSeq&& other) = default;
     SetSeq& operator=(const SetSeq& other) = default;
     SetSeq& operator=(SetSeq&& other) = default;
-    SetSeq(std::initializer_list<value_type> init)
+
+    SetSeq(std::initializer_list<std::set<T>> init)
         : seq(init)
     {
         calcIntersections();
@@ -61,27 +75,77 @@ public:
         return seq.size();
     }
 
-    const_iterator cbegin() noexcept
+    /* Прокси-класс для доступа к элементам последовательности */
+    friend class SetProxy;
+
+    class SetProxy {
+    private:
+        SetSeq& setSeq;
+        std::size_t setIndex;
+
+        std::set<T>& get() const
+        {
+            return setSeq.seq[setIndex];
+        }
+
+        void recalcCurrent()
+        {
+            setSeq.calcIntersections(setIndex);
+        }
+
+    public:
+        SetProxy(SetSeq& theSetSeq, std::size_t index)
+            : setSeq(theSetSeq)
+            , setIndex(index)
+        {
+        }
+
+        /* lvalue */
+        SetProxy& operator=(const SetProxy& rhs)
+        {
+            this->get() = rhs.get();
+            recalcCurrent();
+        }
+        /* lvalue */
+        SetProxy& operator=(std::set<T> rhs)
+        {
+            this->get() = rhs;
+            recalcCurrent();
+        }
+
+        /* Для rvalue */
+        operator std::set<T>() const
+        {
+            return get();
+        }
+
+        std::pair<typename std::set<T>::iterator, bool> insert(const T& value)
+        {
+            auto [it, isChanged] = get().insert(value);
+            if (isChanged) {
+                recalcCurrent();
+            }
+            return { it, isChanged };
+        }
+    };
+
+    /* Доступ к элементам последовательности */
+    const SetProxy operator[](std::size_t index) const
     {
-        return seq.cbegin();
+        return SetProxy(const_cast<SetSeq&>(*this), index);
     }
-    const_iterator cend() noexcept
+    SetProxy operator[](std::size_t index)
     {
-        return seq.cend();
+        return SetProxy(*this, index);
     }
 
-    const_iterator cbeginIntersections() const noexcept
+    typename Container::const_iterator begin()
     {
-        return pairsIntersecions.cbegin();
+        return pairsIntersections.begin();
     }
-    const_iterator cendIntersections() const noexcept
+    typename Container::const_iterator end()
     {
-        return pairsIntersecions.cend();
-    }
-
-    const_reference operator[](size_type pos) const
-    {
-        return seq[pos];
+        return pairsIntersections.end();
     }
 };
 
